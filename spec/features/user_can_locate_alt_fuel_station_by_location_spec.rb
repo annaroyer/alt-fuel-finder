2b90940733177ba22991fc4f56e957d8eb0079f7
Anna Royer <anna.royer@colorado.edu>
2018-03-28 09:26:50 -0600

    add spec user sees the title of the index page

--- spec/features/user_can_locate_alt_fuel_station_by_location_spec.rb
+++ spec/features/user_can_locate_alt_fuel_station_by_location_spec.rb
@@ -9,12 +9,13 @@ describe 'As a user' do
         fill_in :q, with: '80203'
         click_on 'Locate'
 
-        expect(current_path).to eq('/search?q=80203')
+        expect(current_path).to eq('/search')
+        expect(page).to have_content('10 Closest Stations within 6 Miles of 80203')
       end
     end
   end
 end
-#
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+
+#'Then I should see a list of the 10 closest stations within 6 miles sorted by distance'
 # And the stations should be limited to Electric and Propane
 # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
