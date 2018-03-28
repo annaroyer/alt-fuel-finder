d02ab8d5ea8b0834e0c2c1ea53159de3cc70159d
Anna Royer <anna.royer@colorado.edu>
2018-03-28 10:24:20 -0600

    add iterating over returned json and creating stations to index action in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
     @zipcode = params[:q]
+    @stations = NrelService.get_alt_fuel_stations(@zipcode)[:fuel_stations].map do |raw_station|
+      Station.new(raw_station)
+    end
   end
 end
